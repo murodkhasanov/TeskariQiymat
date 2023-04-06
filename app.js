a3120e06b5d2e76e0ba95a4e2ef8d20bcc8e95fb
murodkhasanov <tt1953385@gmail.com>
2023-04-07 02:18:16 +0500

    Sozni teskarisini qaytaradigan dastur

---
+++ app.js
@@ -0,0 +1,16 @@
+//Kiritilgan Number yoki String turdagi qiymatlarni teskarisini qaytaruvchi dastur.
+
+let nome = "murod";
+console.log(nome.split('').reverse().join(''));
+
+// let res = nome.join('')
+// let row = res.sort()
+// let now = row.split()
+// console.log(now);
+
+
+// let res = nome.split('')
+// res = nome.split('')
+// let row = res.join('')
+// row = res.join('')
+// console.log(row.reverse());
